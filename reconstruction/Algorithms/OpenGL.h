9f90c592e1733eb6876d41ad4ea82a965ea69171
Murillo Bouzon <murillo.bouzon@hotmail.com>
2018-08-27 19:25:48 -0300

    Desenho da glut

--- reconstruction/Algorithms/OpenGL.h
+++ reconstruction/Algorithms/OpenGL.h
@@ -2,7 +2,7 @@
 #define ALGORITHMS_OPEN_GL
 
 #include "GL/freeglut.h"
-
+#include "GL/glut.h"
 namespace Algorithms {
 
 	// Colocar todas as classes que serão usadas do OpenGL
--- reconstruction/Services/RenderService.cpp
+++ reconstruction/Services/RenderService.cpp
@@ -18,20 +18,65 @@ namespace Services {
 
 	void display(void) {
 		
-		glutSolidTeapot(40.0);
+		//glutSolidTeapot(40.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* draw white polygon (rectangle) with corners at
+
+		* (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
+
+		*/
+
+		glColor3f(0.50, 0.40, 0.0);
+
+		glBegin(GL_POLYGON);
+
+		glVertex3f(0.25, 0.25, 0.0);
+
+		glVertex3f(0.75, 0.25, 0.0);
+
+		glVertex3f(0.75, 0.75, 0.0);
+
+		glVertex3f(0.25, 0.75, 0.0);
+
+		glEnd();
+
+
+		/* don't wait!
+
+		* start processing buffered OpenGL routines
+
+		*/
+
+		glFlush();
+	}
+	void init() {
+		glClearColor(0.0, 0.0, 0.0, 0.0);
+
+		/* initialize viewing values */
+
+		glMatrixMode(GL_PROJECTION);
+
+		glLoadIdentity();
+
+		glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 	}
 
 	void RenderService::Execute(int *argc, char **argv)
 	{
-		glutInit(argc, argv);
-		glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGB);
-		glutInitWindowPosition(500, 500);
-		glutInitWindowSize(800, 600);
-		glutCreateWindow("OpenGL First Window");
 
-		glEnable(GL_DEPTH_TEST);
-		glClearColor(1.0, 0.0, 0.0, 1.0);
-		// register callbacks
+		glutInit(argc, argv);
+
+		glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+		glutInitWindowSize(250, 250);
+
+		glutInitWindowPosition(100, 100);
+
+		glutCreateWindow("hello");
+
+		init();
+
 		glutDisplayFunc(display);
 
 		glutMainLoop();
