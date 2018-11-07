62718459824d39513cc4095b2da86e2cccf0639a
Gabriel <medeiros_mg@hotmail.com>
2018-11-07 21:25:50 -0200

    Reconstruction compare created

---
+++ Reconstruction/Services/ReconstructionCompareService.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "ReconstructionCompareService.h"
+
+namespace Services {
+
+	ReconstructionCompareService::ReconstructionCompareService()
+	{
+		_openCv = new OpenCV();
+	}
+
+	ReconstructionCompareService::ReconstructionCompareService(OpenCV * openCv)
+	{
+		_openCv = openCv;
+	}
+
+
+	ReconstructionCompareService::~ReconstructionCompareService()
+	{
+	}
+
+	void ReconstructionCompareService::Execute()
+	{
+		
+	}
+}
